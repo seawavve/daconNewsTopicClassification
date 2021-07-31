f2a1d97faafd786066c8cfdea7eeb2617101b984
jundev1l2l <58555428+jundev1l2l@users.noreply.github.com>
2021-07-31 09:48:27 +0900

    .

--- src/model.py
+++ src/model.py
@@ -7,8 +7,7 @@ class BERTClassifier(nn.Module):
                  bert,
                  hidden_size=768,
                  num_classes=7,
-                 dr_rate=None,
-                 params=None):
+                 dr_rate=None):
         super(BERTClassifier, self).__init__()
         self.bert = bert
         self.dr_rate = dr_rate
@@ -26,9 +25,11 @@ class BERTClassifier(nn.Module):
 
     def forward(self, token_ids, valid_length, segment_ids):
         attention_mask = self.gen_attention_mask(token_ids, valid_length)
-        _, pooler = self.bert(input_ids=token_ids, token_type_ids=segment_ids.long(),
-                              attention_mask=attention_mask.float().to(token_ids.device),
-                              return_dict=False)
+        _, pooler = self.bert(
+            input_ids=token_ids,
+            token_type_ids=segment_ids.long(),
+            attention_mask=attention_mask.float().to(token_ids.device)
+        )
         if self.dr_rate:
             out = self.dropout(pooler)
         return self.classifier(out)
